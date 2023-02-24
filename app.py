f5558657c04f031ededb01ce25bd8d2b3e126f52
AvirukBasak <avirukbasak@gmail.com>
2023-02-24 12:51:15 +0530

    n/a

--- app.py
+++ app.py
@@ -2,10 +2,8 @@ import streamlit as st
 
 st.header('Demo Echo App')
 
-def txt_changed():
-    st.write('', txt)
-
 txt = st.text_input('Input',
-    placeholder = 'Enter any text',
-    on_change = txt_changed
+    placeholder = 'Enter any text'
 )
+
+st.write('', txt)

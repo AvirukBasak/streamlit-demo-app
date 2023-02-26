ce639d1a638fbbf3ebb9de1e551a54640888c5ab
AvirukBasak <avirukbasak@gmail.com>
2023-02-26 21:59:14 +0530

    na

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import streamlit as st
 
 st.set_page_config(
-        page_title= "My Demo App",
-        page_icon="📁")
+    page_title='My Demo App',
+    page_icon='📁'
+)
 
 md = open('res/profile.md', 'r')
 data = md.read()

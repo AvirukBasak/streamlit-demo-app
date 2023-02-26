2c791223072324c6c709b06c7ef5e9667ee9e5ab
AvirukBasak <avirukbasak@gmail.com>
2023-02-26 21:55:41 +0530

    updated code based on docs for st.markdown

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 import streamlit as st
 
+st.set_page_config(
+        page_title= "My Demo App",
+        page_icon="📁")
+
 md = open('res/profile.md', 'r')
 data = md.read()
 md.close()
 
-st.markdown(str(data))
+st.markdown(str(data), unsafe_allow_html=True)
